a6dda7bab3ab320944948d2ff6332d5101a2bedf
ukashasohail <ukasha909@outlook.com>
2019-03-20 04:57:42 +0500

    code04

--- code04.asm
+++ code04.asm
@@ -7,6 +7,8 @@ wt0: .asciiz "***Welcome "
 wt1: .asciiz "! You are "
 wt2: .asciiz "years old now. Stay Blessed***"
 
+temp0: .asciiz ""
+
 .text
 
 .globl main
@@ -19,7 +21,38 @@ la $a0, str0
 syscall
 
 li $v0,8
-la $a0, 
+la $a0, temp0
+li $a1,20
+syscall
+
+li $v0,4
+la $a0,str1
+syscall
+
+li $v0,5
+syscall
+
+move $t0,$v0
+
+li $v0,4
+la $a0,wt0
+syscall
+
+li $v0,4
+la $a0,temp0
+syscall
+
+li $v0,4
+la $a0,wt1
+syscall
+
+li $v0,1
+move $a0,$t0
+syscall
+
+li $v0,4
+la $a0,wt2
+syscall
 
 li $v0,10 #exiting 
 syscall
