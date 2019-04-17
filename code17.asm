e59d15dfa102493b4e03368524ded2edd09cc4b4
ukasha <ukasha909@outlook.com>
2019-04-17 07:28:30 +0500

    code8

---
+++ code17.asm
@@ -0,0 +1,68 @@
+.data
+
+input0: .asciiz "Enter Number: "
+output0: .asciiz "Number is Prime."
+output1: .asciiz "Number is Composite."
+
+.text
+.globl main
+.ent main
+main:
+
+#displaying input string
+li $v0, 4
+la $a0, input0
+syscall
+
+#reading input
+li $v0, 5
+syscall
+
+# $s0=n
+move $t1, $v0
+
+addi $t7, $0, 1
+
+#if n=1 then it is composite
+beq $t1, $t7, composite 
+
+#t1 =0
+addi $t0, $0, 0
+
+#$t2 =2
+addi $t2, $0, 2
+
+#s0/2
+div $t1,$t2
+mflo $s1
+
+loop:
+
+slt $s2, $t2, $s1
+beq $s2, $0, prime 
+
+div $t1, $t2
+mfhi $t6
+
+beq $t6, $0, composite
+addi $t2, $t2, 1
+
+j loop
+
+composite:
+
+li $v0, 4
+la $a0,output1
+syscall
+
+j goto
+
+prime:
+li $v0, 4
+la $a0, output0
+syscall
+
+goto:
+
+jr $ra
+.end main
